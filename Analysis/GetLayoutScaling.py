2eaf843ed51849bdbb9cc8d737cad521868d25f7
simonchiche <simon.chiche@iap.fr>
2024-08-12 19:03:57 +0200

    updates of the layout scaling scripts

--- Analysis/GetLayoutScaling.py
+++ Analysis/GetLayoutScaling.py
@@ -7,7 +7,6 @@ Created on Mon May 20 10:38:03 2024
 """
 
 
-
 import numpy as np
 
 
@@ -39,31 +38,6 @@ def GetZenithScaling(theta):
     k= (1 + 0.308*theta*np.pi/180.0)/np.cos(theta*np.pi/180.0)
     return k
 
-def GetDepthScalingOld(Depth):
-
-    p0 = 31
-    p1 = 2.813e-5
-    p2 = 3.07
-
-    r0 = p0
-
-    r = p0 + p1*Depth**p2
-
-    k = r/r0
-
-    return k
-
-def GetEnergyScalingOld(E):
-
-    E0 = 1e16
-    k = (E/E0)**0.5
-
-    return k
-
-def GetZenithScalingOld(theta):
-
-    k= 1/np.cos(theta*np.pi/180.0)
-    return k
 
 # =============================================================================
 #                               Tests
@@ -74,15 +48,4 @@ def ScalingFactor(E, theta, Depth):
     k= GetDepthScaling(Depth)*GetEnergyScaling(E)*GetZenithScaling(theta)
     
     return k
-
-
-
-ScalingFactor(1e16, 0, 0)
-
-
-theta_all = np.linspace(0,50, 20)
-energy_all = np.array([1e16, 10**16.5, 10**17, 10**17.5, 10**18])
-depth_all = np.array([0, 40, 60, 80, 100])
-
-
 
---
+++ Analysis/TestnewScaling.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 17:26:06 2024
+
+@author: chiche
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from GetLayoutScaling import GetDepthScaling, GetEnergyScaling, GetZenithScaling, ScalingFactor
+
+def GetDepthScalingOld(Depth):
+
+    p0 = 31
+    p1 = 2.813e-5
+    p2 = 3.07
+
+    r0 = p0
+
+    r = p0 + p1*Depth**p2
+
+    k = r/r0
+
+    return k
+
+def GetEnergyScalingOld(E):
+
+    E0 = 1e16
+    k = (E/E0)**0.5
+
+    return k
+
+def GetZenithScalingOld(theta):
+
+    k= 1/np.cos(theta*np.pi/180.0)
+    return k
+
+
+def ScalingFactorOld(E, theta, Depth):
+    
+    k= GetDepthScalingOld(Depth)*GetEnergyScalingOld(E)*GetZenithScalingOld(theta)
+    
+    return k
+
+
+
+
+ScalingFactor(1e16, 0, 0)
+ScalingFactorOld(1e16, 0, 0)
+
+
+theta_all = np.linspace(0,50, 20)
+energy_all = np.array([1e16, 10**16.5, 10**17, 10**17.5, 10**18])
+depth_all = np.array([0, 40, 60, 80, 100])
+
+
+ktheta = np.zeros(len(theta_all))
+ke  = np.zeros(len(energy_all))
+kdepth =  np.zeros(len(depth_all))
+
+kthetaOld = np.zeros(len(theta_all))
+keOld = np.zeros(len(energy_all))
+kdepthOld =  np.zeros(len(depth_all))
+
+for i in range(len(ktheta)):
+   
+    ktheta[i] = ScalingFactor(1e16, theta_all[i], 0)
+    kthetaOld[i] = ScalingFactorOld(1e16, theta_all[i], 0)
+
+for i in range(len(ke)):
+    ke[i] = ScalingFactor(energy_all[i], 0, 0)
+    keOld[i] = ScalingFactorOld(energy_all[i], 0, 0)
+    
+for i in range(len(kdepth)):
+    kdepth[i] = ScalingFactor(1e16, 0, depth_all[i])
+    kdepthOld[i] = ScalingFactorOld(1e16, 0, depth_all[i])
+      
+    
+plt.scatter(theta_all, ktheta)  
+plt.scatter(theta_all, kthetaOld) 
+plt.show()  
+
+plt.scatter(energy_all, ke)  
+plt.scatter(energy_all, keOld) 
+plt.show()  
+
+plt.scatter(depth_all, kdepth)  
+plt.scatter(depth_all, kdepthOld) 
+plt.show()  
+ 
+
+    
